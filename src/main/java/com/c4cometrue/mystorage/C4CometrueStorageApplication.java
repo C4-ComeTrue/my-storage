6fa84d63fc8049e2a263eca3824742bc28518908
VSFe <klm03025@gmail.com>
2023-10-03 15:37:58 +0900

    chore: initial commit

---
+++ src/main/java/com/c4cometrue/mystorage/C4CometrueStorageApplication.java
@@ -0,0 +1,13 @@
+package com.c4cometrue.mystorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C4CometrueStorageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C4CometrueStorageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/c4cometrue/mystorage/C4CometrueStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.c4cometrue.mystorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C4CometrueStorageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
