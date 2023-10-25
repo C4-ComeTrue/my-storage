1a22afa0fbe62f1ebaf47b3d28263fb61ae57ff3
semi-cloud <sksmsrkdtpal@naver.com>
2023-10-26 00:57:41 +0900

    refactor: 불필요한 파일 경로 검증 메서드 제거

--- src/main/java/com/c4cometrue/mystorage/service/FileService.java
+++ src/main/java/com/c4cometrue/mystorage/service/FileService.java
@@ -1,10 +1,10 @@
 package com.c4cometrue.mystorage.service;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,11 +25,12 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FileService {
 
-	private static final String UPLOAD_DIR = "/Users/kangsemi/Desktop/git/files/";
-
 	private final FileMetaDataRepository fileMetaDataRepository;
 
 	private final FileUtil fileUtil;
+
+	@Value("file.upload-dir")
+	private String uploadDir;
 
 	/**
 	 * 파일 업로드
@@ -61,24 +62,7 @@ public class FileService {
 
 	private String getFullUploadFilePath(String originalFileName) {
 		String uploadName = UUID.randomUUID() + originalFileName;
-		String fullUploadPAth = UPLOAD_DIR + uploadName;
-		validateFileUploadPath(fullUploadPAth);
-		return fullUploadPAth;
-	}
-
-	private void validateFileUploadPath(String path) {
-		try {
-			File file = new File(path);
-			String canonicalDestPath = file.getCanonicalPath();
-
-			if (!canonicalDestPath.startsWith(UPLOAD_DIR)) {
-				throw new BusinessException(ErrorCode.INVALID_FILE_PATH);
-			}
-
-			file.deleteOnExit();
-		} catch (IOException ex) {
-			throw new BusinessException(ErrorCode.FILE_UPLOAD_FAILED, ex);
-		}
+		return uploadDir + uploadName;
 	}
 
 	private FileMetaData saveFileMetaData(MultipartFile file, long userId, String uploadFileName) {
