71286b16e2774371e5cee742a748e90aa7076bd3
pear96 <bhaeun1105@gmail.com>
2023-12-06 19:02:32 +0900

    refactor : 사용자 이름에 Unique 적용
    동시성 문제를 해결할 방법을 고민하다가 사용자 이름이 동시에 겹치는 경우는 흔하지 않을 것이라고 생각하여
    락을 사용하는 대신 Unique를 적용했습니다.
    낙관적 락을 사용하지 않은 이유는 예외 발생시의 롤백, 처리 로직이 필요하기 때문입니다.

--- src/main/java/com/c4cometrue/mystorage/entity/UserData.java
+++ src/main/java/com/c4cometrue/mystorage/entity/UserData.java
@@ -1,5 +1,6 @@
 package com.c4cometrue.mystorage.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ public class UserData {
 	private Long userId;
 
 	@NotBlank(message = "user name is blank")
+	@Column(unique = true)
 	private String userName;
 
 	public UserData(String userName) {
