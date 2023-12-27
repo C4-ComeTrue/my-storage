15ad8be6bc8da439712746c82df3592ef683eae1
kcc <g2c1010@naver.com>
2023-12-28 00:31:32 +0900

    chore : 사용하지 않는 파일 정리

--- src/main/java/com/c4cometrue/mystorage/meta/StorageStrategy.java
+++
@@ -1,7 +0,0 @@
-package com.c4cometrue.mystorage.meta;
-
-import com.c4cometrue.mystorage.meta.dto.CursorMetaResponse;
-
-public interface StorageStrategy {
-	CursorMetaResponse getContents(Long parentId, Long cursorId, Long userId, Integer contentsSize);
-}
